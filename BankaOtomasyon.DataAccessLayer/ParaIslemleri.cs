0e0c9d0f2aad5c1e8c5c5c0cad040f5ed3b650d3
Hamdi DAMAR <39102004+hamdidamar@users.noreply.github.com>
2019-09-03 00:49:05 +0300

    Update ParaIslemleri.cs

--- BankaOtomasyon.DataAccessLayer/ParaIslemleri.cs
+++ BankaOtomasyon.DataAccessLayer/ParaIslemleri.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using BankaOtomasyon.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,26 @@ namespace BankaOtomasyon.DataAccessLayer
 {
    public class ParaIslemleri
     {
-
+        Hesaplar hesap;
+        HesapIslemleri hesapislem;
+        public ParaIslemleri()
+        {
+            hesap = new Hesaplar();
+            hesapislem = new HesapIslemleri();
+        }
+        public void paracek(decimal cekilecektutar)
+        {
+            hesap.HesapBakiye -= cekilecektutar;
+            hesapislem.HesapGuncelle(hesap);
+        }
+        public void parayatir(decimal yatirilacaktutar)
+        {
+            hesap.HesapBakiye += yatirilacaktutar;
+            hesapislem.HesapGuncelle(hesap);
+        }
+        public Hesaplar bakiyesorgula(Hesaplar hesap)
+        {
+          return  hesapislem.IDHesapGetir(hesap);
+        }
     }
 }
