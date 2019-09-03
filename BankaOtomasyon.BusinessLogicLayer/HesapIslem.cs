501ac904e7e9f40ad1423f0b84f861130511753d
Hamdi DAMAR <39102004+hamdidamar@users.noreply.github.com>
2019-09-04 01:22:37 +0300

    Add files via upload

---
+++ BankaOtomasyon.BusinessLogicLayer/HesapIslem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankaOtomasyon.DataAccessLayer;
+using BankaOtomasyon.Entity;
+using System.Data;
+
+namespace BankaOtomasyon.BusinessLogicLayer
+{
+    public class HesapIslem
+    {
+        HesapIslemleri hesapislem;
+        public HesapIslem()
+        {
+            hesapislem = new HesapIslemleri();
+             
+        }
+        public void HesapEkle(Hesaplar hesap)
+        {
+             hesapislem.HesapEkle(hesap);
+        }
+        public void HesapSil(Hesaplar hesap)
+        {
+            hesapislem.HesapSil(hesap);
+        }
+        public void HesapGuncelle(Hesaplar hesap)
+        {
+            hesapislem.HesapGuncelle(hesap);
+        }
+        public DataTable HesapGetir()
+        {
+            return hesapislem.HesapGetir();
+        }
+        public Hesaplar IDHesapGetir(Hesaplar hesap)
+        {
+            return hesapislem.IDHesapGetir(hesap);
+        }
+    }
+}
---
+++ BankaOtomasyon.BusinessLogicLayer/ParaIslem.cs
@@ -0,0 +1,31 @@
+﻿using BankaOtomasyon.DataAccessLayer;
+using BankaOtomasyon.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankaOtomasyon.BusinessLogicLayer
+{
+   public class ParaIslem
+    {
+        ParaIslemleri paraislem;
+        public ParaIslem()
+        {
+            paraislem = new ParaIslemleri();
+        }
+        public void ParaCek(decimal cekilecektutar)
+        {
+            paraislem.ParaCek(cekilecektutar);
+        }
+        public void ParaYatir(decimal yatirilacaktutar)
+        {
+            paraislem.ParaYatir(yatirilacaktutar);
+        }
+        public Hesaplar BakiyeSorgula(Hesaplar hesap)
+        {
+            return paraislem.BakiyeSorgula(hesap);
+        }
+    }
+}
