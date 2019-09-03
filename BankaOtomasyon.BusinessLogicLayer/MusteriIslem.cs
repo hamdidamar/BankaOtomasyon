9de3a85f22686f68c33edfcd53353fbc8cafdb10
Hamdi DAMAR <39102004+hamdidamar@users.noreply.github.com>
2019-09-04 01:22:14 +0300

    Update MusteriIslem.cs

--- BankaOtomasyon.BusinessLogicLayer/MusteriIslem.cs
+++ BankaOtomasyon.BusinessLogicLayer/MusteriIslem.cs
@@ -1,12 +1,36 @@
-﻿using System;
+using BankaOtomasyon.DataAccessLayer;
+using BankaOtomasyon.Entity;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BankaOtomasyon.BusinessLogicLayer
 {
-    class MusteriIslem
+   public class MusteriIslem
     {
+        MusteriIslemleri musteriislem;
+        public MusteriIslem()
+        {
+            musteriislem = new MusteriIslemleri();
+        }
+        public void MusteriEkle(Musteriler musteri)
+        {
+            musteriislem.MusteriEkle(musteri);
+        }
+        public void MusteriSil(Musteriler musteri)
+        {
+            musteriislem.MusteriSil(musteri);
+        }
+        public void MusteriGuncelle(Musteriler musteri)
+        {
+            musteriislem.MusteriGuncelle(musteri);
+        }
+        public DataTable MusteriGetir()
+        {
+            return musteriislem.MusteriGetir();
+        }
     }
 }
